0c4b28a56108abafeec762c2b012ecbed3481fc9
Sherlockxm <2737618018@qq.com>
2025-08-17 18:13:54 +0800

    Site updated: 2025-08-17 18:13:53

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1755425472570";const preCache = ["/images/taichi.png","/images/gudu.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1755425632802";const preCache = ["/images/taichi.png","/images/gudu.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
